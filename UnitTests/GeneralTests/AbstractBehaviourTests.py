2aa1d1bf8b4749e353d2db22621844c8bb385090
davidvlaminck <davidvlaminck85@gmail.com>
2022-10-28 08:47:16 +0200

    refactored the tests to use AllCasesTestClass

--- UnitTests/GeneralTests/AbstractBehaviourTests.py
+++ UnitTests/GeneralTests/AbstractBehaviourTests.py
@@ -1,9 +1,9 @@
 import unittest
 
-from otlmow_model.Classes.ImplementatieElement.AIMDBStatus import AIMDBStatus
-from otlmow_model.Classes.ImplementatieElement.AIMObject import AIMObject
-from otlmow_model.Classes.ImplementatieElement.AIMToestand import AIMToestand
-from otlmow_model.Classes.Onderdeel.Contactor import Contactor
+from UnitTests.TestClasses.Classes.ImplementatieElement.AIMDBStatus import AIMDBStatus
+from UnitTests.TestClasses.Classes.ImplementatieElement.AIMObject import AIMObject
+from UnitTests.TestClasses.Classes.ImplementatieElement.AIMToestand import AIMToestand
+from UnitTests.TestClasses.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
 
 
 class AbstractBehaviour(unittest.TestCase):
@@ -16,21 +16,21 @@ class AbstractBehaviour(unittest.TestCase):
             AIMObject()
 
     def test_TestAssignmentsOnSameClasses(self):
-        contactor = Contactor()
-        self.assertTrue(contactor.typeURI == "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Contactor")
-        self.assertTrue(isinstance(contactor, Contactor))
-        self.assertTrue(isinstance(contactor, AIMObject))
-        contactor2 = Contactor()
+        instance = AllCasesTestClass()
+        self.assertTrue(instance.typeURI == "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass")
+        self.assertTrue(isinstance(instance, AllCasesTestClass))
+        self.assertTrue(isinstance(instance, AIMObject))
+        instance2 = AllCasesTestClass()
 
-        contactor.notitie = "notitie1"
-        contactor2.notitie = "notitie2"
-        self.assertTrue(contactor.notitie == "notitie1")
-        self.assertTrue(contactor2.notitie == "notitie2")
+        instance.notitie = "notitie1"
+        instance2.notitie = "notitie2"
+        self.assertTrue(instance.notitie == "notitie1")
+        self.assertTrue(instance2.notitie == "notitie2")
 
-        contactor3 = Contactor()
-        self.assertTrue(contactor3.notitie is None)
+        instance3 = AllCasesTestClass()
+        self.assertTrue(instance3.notitie is None)
 
-        contactor3.notitie = "notitie3"
-        self.assertTrue(contactor.notitie == "notitie1")
-        self.assertTrue(contactor2.notitie == "notitie2")
-        self.assertTrue(contactor3.notitie == "notitie3")
+        instance3.notitie = "notitie3"
+        self.assertTrue(instance.notitie == "notitie1")
+        self.assertTrue(instance2.notitie == "notitie2")
+        self.assertTrue(instance3.notitie == "notitie3")
