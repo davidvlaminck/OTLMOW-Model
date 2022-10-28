7e1c0de53cadcea78d353b328d715881f50bad11
davidvlaminck <davidvlaminck85@gmail.com>
2022-10-28 08:46:32 +0200

    removed irrelevant tests

--- UnitTests/GeneralTests/AttributeInfoTests.py
+++ UnitTests/GeneralTests/AttributeInfoTests.py
@@ -1,39 +1,6 @@
 ﻿from unittest import TestCase
 
-from otlmow_model.BaseClasses.OTLObject import OTLObjectHelper
-from otlmow_model.Classes.Onderdeel.Aftakking import Aftakking
-from otlmow_model.Classes.Onderdeel.Verkeersregelaar import Verkeersregelaar
-
 
 class AttributeInfoTests(TestCase):
-    def test_info_empty_class(self):
-        infoString = Aftakking().info()
-        expected = '^information about Aftakking \d{10,13}:\n$'
-        self.assertRegex(infoString, expected)
-
-    def test_info_empty_class2(self):
-        infoString = Verkeersregelaar().info()
-        expected = '^information about Verkeersregelaar \d{10,13}:\n$'
-        self.assertRegex(infoString, expected)
-
-    def test_make_string_version_empty_class(self):
-        v = Verkeersregelaar()
-        string_version = OTLObjectHelper().build_string_version(v, indent=4)
-        expected = ''
-        self.assertEqual(string_version, expected)
-
-    def test_make_string_version_StringField(self):
-        v = Verkeersregelaar()
-        v.naam = 'VR'
-        string_version = OTLObjectHelper().build_string_version(v, indent=4)
-        expected = 'naam : VR'
-        self.assertEqual(string_version, expected)
-
-    def test_make_string_version_DtcIdentificator(self):
-        v = Verkeersregelaar()
-        v.assetId.identificator = 'eigen_id'
-        v.assetId.toegekendDoor = 'AWV'
-        string_version = OTLObjectHelper().build_string_version(v, indent=4)
-        expected = 'assetId.identificator : eigen_id\nassetId.toegekendDoor : AWV'
-        self.assertEqual(string_version, expected)
+    pass
 
