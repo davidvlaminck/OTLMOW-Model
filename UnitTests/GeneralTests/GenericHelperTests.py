4458d66f385bb249c478101912e1a2efaa1d89ed
davidvlaminck <davidvlaminck85@gmail.com>
2022-11-14 15:04:13 +0100

    refactoring of GenericHelper class: - typing hints - functions instead of static class - both printing and counting of assets in a list - support for dotnotation using '.' in remove_duplicates_in_iterable_based_on_property() - raising a NotImplementedError when a value is a list

--- UnitTests/GeneralTests/GenericHelperTests.py
+++ UnitTests/GeneralTests/GenericHelperTests.py
@@ -1,31 +1,54 @@
 from unittest import TestCase
 
-from otlmow_model.Helpers.GenericHelper import GenericHelper
-
-
-class TestClass:
-    def __init__(self):
-        self._a = ''
-
-    @property
-    def a(self):
-        return self._a
-
-    @a.setter
-    def a(self, value):
-        self._a = value
+from UnitTests.TestClasses.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
+from UnitTests.TestClasses.Classes.Onderdeel.AnotherTestClass import AnotherTestClass
+from UnitTests.TestClasses.Classes.Onderdeel.Bevestiging import Bevestiging
+from otlmow_model.Helpers.GenericHelper import count_assets_by_type, \
+    remove_duplicates_in_iterable_based_on_property
 
 
 class GenericHelperTests(TestCase):
+    def test_count_assets_by_type(self):
+        assets = [Bevestiging(), AllCasesTestClass(), AllCasesTestClass(), AnotherTestClass()]
+        expected_dict = {'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Bevestiging': 1,
+                         'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass': 2,
+                         'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AnotherTestClass': 1}
+        self.assertDictEqual(expected_dict, count_assets_by_type(assets))
+
     def test_remove_duplicates_in_iterable_based_on_property(self):
-        t = TestClass()
-        t.a = '1'
-        u = TestClass()
-        u.a = '1'
-        v = TestClass()
-        v.a = '2'
-        testset = [t, u, v]
+        t = AllCasesTestClass()
+        t.naam = '1'
+        u = AllCasesTestClass()
+        u.naam = '1'
+        v = AllCasesTestClass()
+        v.naam = '2'
+        w = AllCasesTestClass()
+        testset = [t, u, v, w]
         expected_result = [t, v]
-        result = GenericHelper.remove_duplicates_in_iterable_based_on_property(testset, 'a')
+        result = remove_duplicates_in_iterable_based_on_property(testset, 'naam')
 
         self.assertListEqual(expected_result, result)
+
+    def test_remove_duplicates_in_iterable_based_on_complex_property(self):
+        t = AllCasesTestClass()
+        t.testComplexType.testComplexType2.testStringField = '1'
+        u = AllCasesTestClass()
+        u.testComplexType.testComplexType2.testStringField = '1'
+        v = AllCasesTestClass()
+        v.testComplexType.testComplexType2.testStringField = '2'
+        testset = [t, u, v]
+        expected_result = [t, v]
+        result = remove_duplicates_in_iterable_based_on_property(testset,
+                                                                 'testComplexType.testComplexType2.testStringField')
+
+        self.assertListEqual(expected_result, result)
+
+    def test_remove_duplicates_in_iterable_based_on_complex_property_list(self):
+        t = AllCasesTestClass()
+        t.testComplexType._testComplexType2MetKard.add_empty_value()
+        t.testComplexType.testComplexType2MetKard[0].testStringField = '1'
+        testset = [t]
+
+        with self.assertRaises(NotImplementedError):
+            remove_duplicates_in_iterable_based_on_property(testset,
+                                                            'testComplexType.testComplexType2MetKard.testStringField')
