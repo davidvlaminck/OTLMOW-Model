8aa1df259e2c24d6ba3d9fc31c0718dafbcf9ef4
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-04-06 21:10:13 +0200

    get size of objects in memory

--- SampleCode/printing_instances.py
+++ SampleCode/printing_instances.py
@@ -1,8 +1,14 @@
+import datetime
+import sys
+
+from otlmow_model.BaseClasses.OTLObject import create_dict_from_asset
 from otlmow_model.Classes.Onderdeel.Bevestiging import Bevestiging
 from otlmow_model.Classes.Onderdeel.Verkeersregelaar import Verkeersregelaar
 from otlmow_model.Classes.Onderdeel.Wegkantkast import Wegkantkast
 
 from otlmow_model.Helpers.RelationCreator import create_relation
+
+from pysize import get_size
 
 if __name__ == "__main__":
     from otlmow_model.Helpers.AssetCreator import dynamic_create_instance_from_ns_and_name
@@ -11,6 +17,16 @@ if __name__ == "__main__":
 
     vr = Verkeersregelaar()
     vr.assetId.identificator = '00000000-0000-0000-0000-000000000001'
+    vr.toestand = 'in-gebruik'
+    vr.theoretischeLevensduur.waarde = 360
+    vr.isActief = True
+    vr.datumOprichtingObject = datetime.datetime(2022, 12, 12)
+
+    d = create_dict_from_asset(vr)
+
+    print(get_size(vr))
+    print(get_size(d))
+
     relation = create_relation(source=vr, target_typeURI=Wegkantkast.typeURI,
                                target_uuid='00000000-0000-0000-0000-000000000002', relation_type=Bevestiging)
     print(relation)
