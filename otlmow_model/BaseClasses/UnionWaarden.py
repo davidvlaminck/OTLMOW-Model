8ec2c4e624b9759512cb1f9912ff9697894ba9d1
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-04-06 21:49:24 +0200

    using __iter__ instead of vars

--- otlmow_model/BaseClasses/UnionWaarden.py
+++ otlmow_model/BaseClasses/UnionWaarden.py
@@ -5,14 +5,12 @@ class UnionWaarden(WaardenObject):
     def __init__(self):
         WaardenObject.__init__(self)
 
-    def clear_other_props(self, prop_name):
-        other_prop_keys = list(filter(lambda x: x != prop_name, vars(self).keys()))
-        for key in other_prop_keys:
-            if key == '_parent' or key[0] != '_':
+    def clear_other_props(self, prop_name: str):
+        prop_name = prop_name[1:]
+        for attribute in self:
+            if attribute.naam == prop_name:
                 continue
-            attr = getattr(self, key)
-            if attr.field.waardeObject is not None and not attr.field.waarde_shortcut_applicable:
-                attr = getattr(self, key[1:])
-                setattr(attr, 'waarde', None)
+            if attribute.field.waardeObject is not None and not attribute.field.waarde_shortcut_applicable:
+                setattr(attribute.waarde, 'waarde', None)
             else:
-                setattr(self, key[1:], None)
+                setattr(attribute, 'waarde', None)
