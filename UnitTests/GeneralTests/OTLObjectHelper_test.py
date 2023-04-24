e0ef8f85bf1cabbccffd3712112bd773a55484b9
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-04-24 23:51:57 +0200

    added list comparison helper functions

--- UnitTests/GeneralTests/OTLObjectHelper_test.py
+++ UnitTests/GeneralTests/OTLObjectHelper_test.py
@@ -3,7 +3,8 @@ import pytest
 from UnitTests.TestClasses.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
 from UnitTests.TestClasses.Classes.Onderdeel.AnotherTestClass import AnotherTestClass
 from UnitTests.TestClasses.Classes.Onderdeel.Bevestiging import Bevestiging
-from otlmow_model.Helpers.OTLObjectHelper import count_assets_by_type, remove_duplicates_in_iterable_based_on_property
+from otlmow_model.Helpers.OTLObjectHelper import count_assets_by_type, remove_duplicates_in_iterable_based_on_property, \
+    compare_two_lists_of_objects_object_level, verify_asset_id_is_unique_within_list, compare_two_lists_of_objects_attribute_level, custom_dict_diff
 
 
 def test_count_assets_by_type():
@@ -53,3 +54,174 @@ def test_remove_duplicates_in_iterable_based_on_complex_property_list():
     with pytest.raises(NotImplementedError):
         remove_duplicates_in_iterable_based_on_property(testset,
                                                         'testComplexType.testComplexType2MetKard.testStringField')
+
+
+def test_get_diff_from_two_lists_empty_one():
+    instance = AllCasesTestClass()
+    list_one = []
+    list_two = [instance]
+
+    expected = [instance]
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_get_diff_from_two_lists_one_one():
+    instance = AllCasesTestClass()
+    list_one = [instance]
+    list_two = [instance]
+
+    expected = []
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_get_diff_from_two_lists_one_one_using_dict():
+    d = {'toestand': 'in-gebruik'}
+    list_one = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+    list_two = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+
+    expected = []
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_get_diff_from_two_lists_one_two():
+    instance = AllCasesTestClass()
+    list_one = [instance]
+    list_two = [instance, instance]
+
+    expected = []
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_get_diff_from_two_lists_one_empty():
+    instance = AllCasesTestClass()
+    list_one = [instance]
+    list_two = []
+
+    expected = []
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_get_diff_from_two_lists_one_one_different():
+    instance = AllCasesTestClass()
+    list_one = [instance]
+    instance2 = AllCasesTestClass()
+    instance2.toestand = 'in-gebruik'
+    list_two = [instance2]
+
+    expected = [instance2]
+    result = compare_two_lists_of_objects_object_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_verify_asset_id_is_unique_correct_dict_list():
+    dict_list = [{'assetId': {'identificator': '1'}}, {'assetId': {'identificator': '2'}}]
+    assert verify_asset_id_is_unique_within_list(dict_list)
+
+
+def test_verify_asset_id_is_unique_incorrect_dict_lists(subtests):
+    with subtests.test(msg='not unique ids'):
+        with pytest.raises(ValueError):
+            verify_asset_id_is_unique_within_list([{'assetId': {'identificator': '1'}}, {'assetId': {'identificator': '1'}}])
+    with subtests.test(msg='None in asset id'):
+        with pytest.raises(ValueError):
+            verify_asset_id_is_unique_within_list([{'assetId': {'identificator': None}}])
+
+
+def test_compare_two_lists_return_minimal_identical_objects():
+    d = {'toestand': 'in-gebruik', 'assetId': {'identificator': '1'}}
+    list_one = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+    list_two = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+
+    expected = []
+    result = compare_two_lists_of_objects_attribute_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_compare_two_lists_return_minimal_missing_object():
+    d = {'toestand': 'in-gebruik', 'assetId': {'identificator': '1'}}
+    list_one = []
+    list_two = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+
+    expected = [AllCasesTestClass.from_dict(d, directory='UnitTests.TestClasses.Classes')]
+    result = compare_two_lists_of_objects_attribute_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_compare_two_lists_return_minimal_changed_attribute():
+    d1 = {'toestand': 'gepland', 'geometry': 'POINT Z (200000 200000 0)', 'assetId': {'identificator': '1'}}
+    list_one = [AllCasesTestClass.from_dict(d1, directory='UnitTests.TestClasses.Classes')]
+    d2 = {'toestand': 'in-gebruik', 'geometry': 'POINT Z (200000 200000 0)', 'assetId': {'identificator': '1'}}
+    list_two = [AllCasesTestClass.from_dict(d2, directory='UnitTests.TestClasses.Classes')]
+
+    expected = [AllCasesTestClass.from_dict(
+        {'toestand': 'in-gebruik', 'assetId': {'identificator': '1'}},
+        directory='UnitTests.TestClasses.Classes')]
+    result = compare_two_lists_of_objects_attribute_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_compare_two_lists_return_minimal_missing_attribute():
+    d1 = {'geometry': 'POINT Z (200000 200000 0)', 'assetId': {'identificator': '1'}}
+    list_one = [AllCasesTestClass.from_dict(d1, directory='UnitTests.TestClasses.Classes')]
+    d2 = {'toestand': 'in-gebruik', 'geometry': 'POINT Z (200000 200000 0)', 'assetId': {'identificator': '1'}}
+    list_two = [AllCasesTestClass.from_dict(d2, directory='UnitTests.TestClasses.Classes')]
+
+    expected = [AllCasesTestClass.from_dict(
+        {'toestand': 'in-gebruik', 'assetId': {'identificator': '1'}},
+        directory='UnitTests.TestClasses.Classes')]
+    result = compare_two_lists_of_objects_attribute_level(list_one, list_two, directory='UnitTests.TestClasses.Classes')
+    assert result == expected
+
+
+def test_custom_dict_diff():
+    d1 = {}
+    d2 = {'a': 1}
+    result1 = custom_dict_diff(d1, d2)
+    expected1 = {'a': 1}
+    result2 = custom_dict_diff(d2, d1)
+    expected2 = {}
+    assert result1 == expected1
+    assert result2 == expected2
+
+    d1 = {'a': 1, 'c': 3}
+    d2 = {'a': 1, 'b': 2}
+    result1 = custom_dict_diff(d1, d2)
+    expected1 = {'b': 2}
+    result2 = custom_dict_diff(d2, d1)
+    expected2 = {'c': 3}
+    assert result1 == expected1
+    assert result2 == expected2
+
+    d1 = {'a': [1, 2]}
+    d2 = {'a': [1]}
+    result1 = custom_dict_diff(d1, d2)
+    expected1 = {'a': [1]}
+    result2 = custom_dict_diff(d2, d1)
+    expected2 = {'a': [1, 2]}
+    assert result1 == expected1
+    assert result2 == expected2
+
+    d1 = {'a': {'a1': 1}}
+    d2 = {'a': {'a1': 1, 'a2': 2}}
+    result1 = custom_dict_diff(d1, d2)
+    expected1 = {'a': {'a2': 2}}
+    result2 = custom_dict_diff(d2, d1)
+    expected2 = {}
+    assert result1 == expected1
+    assert result2 == expected2
+
+    d1 = {'a': [{'a1': 1}, {'b': 1}]}
+    d2 = {'a': [{'a1': 1, 'a2': 2}, {'b': 1}, {'c': 3}]}
+    result1 = custom_dict_diff(d1, d2)
+    expected1 = {'a': [{'a1': 1, 'a2': 2}, {'b': 1}, {'c': 3}]}
+    result2 = custom_dict_diff(d2, d1)
+    expected2 = {'a': [{'a1': 1}, {'b': 1}]}
+    assert result1 == expected1
+    assert result2 == expected2
+
+
--- otlmow_model/Helpers/OTLObjectHelper.py
+++ otlmow_model/Helpers/OTLObjectHelper.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
-from typing import Iterable
+from typing import Iterable, List
 
-from otlmow_model.BaseClasses.OTLObject import OTLObject
+from otlmow_model.BaseClasses.OTLObject import OTLObject, create_dict_from_asset
 
 
 def count_assets_by_type(objects: Iterable[OTLObject]) -> defaultdict:
@@ -16,6 +16,7 @@ def print_overview_assets(objects: Iterable[OTLObject]) -> None:
         print(f'counting {str(v)} assets of type {k}')
 
 
+# TODO move to converter (this is using a dotnotation)
 def remove_duplicates_in_iterable_based_on_property(iterable: Iterable[OTLObject], property_name: str) -> []:
     d = {}
     for asset in iterable:
@@ -38,3 +39,77 @@ def remove_duplicates_in_iterable_based_on_property(iterable: Iterable[OTLObject
             if item_prop_str not in d:
                 d[item_prop_str] = asset
     return list(d.values())
+
+
+def compare_two_lists_of_objects_object_level(first_list: List[OTLObject], second_list: List[OTLObject], directory=None
+                                              ) -> List:
+    """Given two lists of objects return the differences from the second list compared to the first list.
+    Returns full objects from the second list when unmatched with the first list. """
+    l1 = list(map(lambda x: create_dict_from_asset(x), first_list))
+    l2 = list(map(lambda x: create_dict_from_asset(x), second_list))
+    diff_list = [d for d in l2 if d not in l1]
+    return list(map(lambda x: OTLObject.from_dict(x, directory), diff_list))
+
+
+def custom_dict_diff(first_dict, second_dict):
+    diff_dict = {}
+    for k, v in second_dict.items():
+        orig_v = first_dict.get(k)
+        if orig_v is None:
+            diff_dict[k] = v
+            continue
+        if orig_v != v:
+            if isinstance(v, dict) and isinstance(orig_v, dict):
+                result_dict = custom_dict_diff(orig_v, v)
+                if result_dict != {}:
+                    diff_dict[k] = custom_dict_diff(orig_v, v)
+            else:
+                diff_dict[k] = v
+    return diff_dict
+
+
+def compare_two_lists_of_objects_attribute_level(first_list: List[OTLObject], second_list: List[OTLObject],
+                                                 directory=None) -> List:
+    """
+    Given two lists of objects return the differences from the second list compared to the first list.
+    Assumes both lists have objects with a unique assetId. Returns partial objects (on attribute level)
+    from the second list when unmatched with the first list. """
+    l1 = list(map(lambda x: create_dict_from_asset(x), first_list))
+    verify_asset_id_is_unique_within_list(l1)
+
+    l2 = list(map(lambda x: create_dict_from_asset(x), second_list))
+    verify_asset_id_is_unique_within_list(l2)
+
+    l1_dict_list = {dict_asset['assetId']['identificator']: dict_asset for dict_asset in l1}
+    l1_dict_list_keys = list(l1_dict_list.keys())
+
+    diff_list = []
+    for d in l2:
+        asset_id = d['assetId']['identificator']
+        if asset_id not in l1_dict_list_keys:
+            diff_list.append(d)
+            continue
+        orig_dict = l1_dict_list[asset_id]
+        if orig_dict == d:
+            continue
+        diff_dict = custom_dict_diff(orig_dict, d)
+        if diff_dict == {}:
+            continue
+        diff_dict['assetId'] = {'identificator': asset_id}
+        diff_dict['typeURI'] = orig_dict['typeURI']
+        diff_list.append(diff_dict)
+
+    return list(map(lambda x: OTLObject.from_dict(x, directory), diff_list))
+
+
+def verify_asset_id_is_unique_within_list(dict_list) -> bool:
+    try:
+        asset_ids_list_1 = list(map(lambda x: x['assetId']['identificator'], dict_list))
+        asset_ids_set_1 = set(asset_ids_list_1)
+        if None in asset_ids_set_1:
+            raise ValueError('This list has a None value for assetId for at least one asset in this list')
+        if len(asset_ids_list_1) != len(asset_ids_set_1):
+            raise ValueError("There are non-unique assetId's in assets in this list")
+        return True
+    except Exception as ex:
+        raise ex
