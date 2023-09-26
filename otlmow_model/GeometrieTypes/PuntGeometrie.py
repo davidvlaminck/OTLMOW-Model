f9e4c9241f6286c0d6085a0e2c270c0c589f5bc0
bertvanovermeir <bert.vanovermeir@gmail.com>
2023-09-26 10:26:15 +0200

    Changed Geometrie classes to abstract - not bug free - needs rework

--- otlmow_model/GeometrieTypes/PuntGeometrie.py
+++ otlmow_model/GeometrieTypes/PuntGeometrie.py
@@ -2,26 +2,11 @@ from abc import abstractmethod, ABC
 
 from otlmow_model.BaseClasses.OTLObject import OTLAttribuut
 from otlmow_model.BaseClasses.WKTField import WKTField
+from otlmow_model.GeometrieTypes.AbstracteGeometrie import AbstracteGeometrie
 
 
-class PuntGeometrie(ABC):
+class PuntGeometrie(AbstracteGeometrie):
     @abstractmethod
     def __init__(self):
-        if not hasattr(self, '_geometry_types'):
-            self._geometry_types = []
+        super().__init__()
         self._geometry_types.append('POINT Z')
-        self._geometry = OTLAttribuut(field=WKTField,
-                                      naam='geometry',
-                                      label='geometry',
-                                      objectUri='https://loc.data.wegenenverkeer.be/ns/implementatieelement#Locatie.geometrie',
-                                      definition='geometry voor DAVIE',
-                                      owner=self)
-
-    @property
-    def geometry(self):
-        """geometry voor DAVIE"""
-        return self._geometry.waarde
-
-    @geometry.setter
-    def geometry(self, value):
-        self._geometry.set_waarde(value, owner=self)
