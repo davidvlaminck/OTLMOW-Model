27947149cfefbbf4a853e1839d4b0cb5f0c12c63
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-06-11 20:50:27 +0200

    updated ModelUpdater

--- UnitTests/ModelUpdater/ModelUpdater_test.py
+++ UnitTests/ModelUpdater/ModelUpdater_test.py
@@ -1,6 +1,8 @@
 ﻿import json
 from pathlib import Path
 
+import pytest
+
 from model_update.ModelUpdater import ModelUpdater
 
 fake_github_root_path = Path(__file__).parent / 'FakeGitHubRoot'
@@ -35,21 +37,33 @@ def generate_version_info(file_path: Path):
         }, file, indent=4)
 
 
+def test_update_model_same_version():
+    version_info_file_path = fake_github_root_path / 'otlmow_model' / 'version_info.json'
+    generate_version_info(version_info_file_path)
+
+    with pytest.raises(ValueError):
+        ModelUpdater(github_root=fake_github_root_path).update_model(
+            otl_version='2.9.0', created_by='david.vlaminck', enums_updated=[])
+
+    version_info_file_path.unlink()
+
+
 def test_update_model_new_version():
     version_info_file_path = fake_github_root_path / 'otlmow_model' / 'version_info.json'
     generate_version_info(version_info_file_path)
 
     ModelUpdater(github_root=fake_github_root_path).update_model(
-        model_version='2.9.6.0', otl_version='2.9.0', created_by='david.vlaminck', updated_class_model=False,
-        updated_enums=False, enums_updated=[])
+        otl_version='2.10.0', created_by='david.vlaminck', enums_updated=[])
 
     with open(version_info_file_path, encoding='utf-8') as file:
         written_data = json.load(file)
 
-    assert written_data['current']['model_version'] == '2.9.6.0'
-    assert written_data['current']['otl_version'] == '2.9.0'
+    assert written_data['current']['model_version'] == '2.10.0.0'
+    assert written_data['current']['otl_version'] == '2.10.0'
     assert written_data['current']['created_by'] == 'david.vlaminck'
-    assert written_data['history']['2.9.6.0']['previous_version'] == '2.9.5.0'
+    assert written_data['history']['2.10.0.0']['previous_version'] == '2.9.5.0'
+    assert written_data['history']['2.10.0.0']['updated_class_model']
+    assert not written_data['history']['2.10.0.0']['updated_enums']
 
     version_info_file_path.unlink()
 
@@ -71,3 +85,20 @@ def test_find_changed_enums():
 
     toestand_file_path.write_text(toestand_orig_file_contents, encoding='utf-8')
     new_file_path.unlink()
+
+
+@pytest.mark.parametrize(
+    "model_version, otl_version, updated_class_model, updated_enums, expected",
+    [
+        ('2.9.0.0', '2.9.0', False, False, '2.9.0.0'),
+        ('2.9.0.0', '2.9.0', False, True, '2.9.0.1'),
+        ('2.9.0.0', '2.10.0', True, True, '2.10.0.0'),
+        ('2.9.1.0', '2.9.1', True, True, '2.9.2.0')
+    ],
+    ids=["no_update", "update_enums", "update_major_version", "update_minor_version"]
+)
+def test_update_model_version(model_version, otl_version, updated_class_model, updated_enums, expected):
+    result = ModelUpdater.update_model_version(model_version=model_version, otl_version=otl_version,
+                                               updated_class_model=updated_class_model, updated_enums=updated_enums)
+    assert result == expected
+
--- model_update/ModelUpdater.py
+++ model_update/ModelUpdater.py
@@ -10,14 +10,20 @@ class ModelUpdater:
     def __init__(self, github_root: Path = GITHUB_ROOT):
         self.github_root = github_root
 
-    def update_model(self, model_version: str, otl_version: str, created_by: str, updated_class_model: bool,
-                     updated_enums: bool, enums_updated: list):
+    def update_model(self, otl_version: str, created_by: str, enums_updated: list):
         version_info_file_path = self.github_root / 'otlmow_model' / 'version_info.json'
         with open(version_info_file_path, 'r', encoding='utf-8') as file:
             version_info = json.load(file)
 
-        current_version = version_info['current']['model_version']
-        if current_version == model_version:
+        current_otl_version = version_info['current']['otl_version']
+        updated_class_model = (current_otl_version != otl_version)
+        updated_enums = len(enums_updated) > 0
+
+        current_model_version = version_info['current']['model_version']
+        model_version = self.update_model_version(updated_class_model=updated_class_model, updated_enums=updated_enums,
+                                                  model_version=current_model_version, otl_version=otl_version)
+
+        if current_model_version == model_version:
             raise ValueError(f'The model version you are trying to update to is the same as the current version: '
                              f'{model_version}')
 
@@ -29,7 +35,7 @@ class ModelUpdater:
         }
 
         version_info['history'][model_version] = {
-            'previous_version': current_version,
+            'previous_version': current_model_version,
             'updated_class_model': updated_class_model,
             'updated_enums': updated_enums,
             'enums_updated': enums_updated
@@ -44,13 +50,30 @@ class ModelUpdater:
         cmd = 'git status --porcelain'
         p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = p.communicate()
-        files_changed_list = stdout.decode("utf-8").splitlines()
+        files_changed_list = stdout.decode().splitlines()
         enums_changed = []
         for entry in files_changed_list:
             xy = entry[:2]
             if 'M' in xy or 'A' in xy:
                 path = entry[3:]
-                if path.startswith('otlmow_model/OtlmowModel/Datatypes/Kl'):
+                if (path.startswith('otlmow_model/OtlmowModel/Datatypes/Kl') or path ==
+                        'otlmow_model/OtlmowModel/Datatypes/AntiParkeerpaalType.py'):
                     enums_changed.append(path[35:-3])
         return enums_changed
 
+    @staticmethod
+    def update_model_version(updated_class_model: bool, updated_enums: bool, model_version: str,
+                             otl_version: str) -> str:
+        major, minor, build, enum = model_version.split('.')
+        otl_major, otl_minor, otl_patch = otl_version.split('.')
+        if updated_class_model:
+            if major == otl_major and minor == otl_minor:
+                build = str(int(build) + 1)
+            else:
+                major = otl_major
+                minor = otl_minor
+                build = '0'
+            enum = '0'
+        elif updated_enums:
+            enum = str(int(enum) + 1)
+        return f'{major}.{minor}.{build}.{enum}'
