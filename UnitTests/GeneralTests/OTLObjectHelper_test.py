ffb2b40435314145e7b7a9ac9130ae14e7449765
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-10-31 17:14:05 +0100

    combine assets functionality

--- UnitTests/GeneralTests/OTLObjectHelper_test.py
+++ UnitTests/GeneralTests/OTLObjectHelper_test.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pytest
+from otlmow_model.OtlmowModel.BaseClasses.OTLObject import create_dict_from_asset
 
 from UnitTests.TestModel.OtlmowModel.Classes.Onderdeel.AllCasesTestClass import AllCasesTestClass
 from UnitTests.TestModel.OtlmowModel.Classes.Onderdeel.AnotherTestClass import AnotherTestClass
@@ -9,7 +10,8 @@ from UnitTests.TestModel.OtlmowModel.Classes.Onderdeel.Voedt import Voedt
 from otlmow_model.OtlmowModel.Helpers.OTLObjectHelper import count_assets_by_type, \
     remove_duplicates_in_iterable_based_on_property, \
     compare_two_lists_of_objects_object_level, verify_asset_id_is_unique_within_list, \
-    compare_two_lists_of_objects_attribute_level, custom_dict_diff, is_relation, is_directional_relation
+    compare_two_lists_of_objects_attribute_level, custom_dict_diff, is_relation, is_directional_relation, \
+    combine_two_asset_instances, combine_assets
 
 model_directory_path = Path(__file__).parent.parent / 'TestModel'
 
@@ -242,3 +244,123 @@ def test_is_directional_relation():
     assert not is_directional_relation(AllCasesTestClass(), model_directory=model_directory_path)
     assert not is_directional_relation(Bevestiging(), model_directory=model_directory_path)
     assert is_directional_relation(Voedt(), model_directory=model_directory_path)
+
+
+def test_combine_two_asset_instances(subtests):
+    empty = AllCasesTestClass()
+    minimum = AllCasesTestClass()
+    minimum.assetId.identificator = '1'
+    minimum_2 = AllCasesTestClass()
+    minimum_2.assetId.identificator = '1'
+    minimum_3 = AllCasesTestClass()
+    minimum_3.assetId.identificator = '2'
+    one_attribute = AllCasesTestClass()
+    one_attribute.assetId.identificator = '1'
+    one_attribute.toestand = 'in-gebruik'
+    two_attributes = AllCasesTestClass()
+    two_attributes.assetId.identificator = '1'
+    two_attributes.toestand = 'in-gebruik'
+    two_attributes.testStringField = 'naam'
+    two_attributes_2 = AllCasesTestClass()
+    two_attributes_2.assetId.identificator = '1'
+    two_attributes_2.isActief = True
+    two_attributes_2.testStringField = 'naam_2'
+    other_type = AnotherTestClass()
+    other_type.assetId.identificator = '1'
+    other_type.deprecatedString = 'deprecated'
+    other_type_2 = AnotherTestClass()
+    other_type_2.assetId.identificator = '1'
+    other_type_2.deprecatedString = 'deprecated'
+
+    with subtests.test(msg='empty arguments'):
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=None, asset2=None)
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=empty, asset2=None)
+
+    with subtests.test(msg='empty asset identificator'):
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=empty, asset2=minimum)
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=minimum, asset2=empty)
+
+    with subtests.test(msg='assets with different identificators'):
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=minimum, asset2=minimum_3)
+
+    with subtests.test(msg='assets that are not the same type'):
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=minimum, asset2=other_type)
+
+    with subtests.test(msg='assets with no attributes'):
+        result = combine_two_asset_instances(asset1=minimum, asset2=minimum_2)
+        assert result == minimum
+
+    with subtests.test(msg='assets with one attribute'):
+        result = combine_two_asset_instances(asset1=minimum, asset2=one_attribute)
+        assert result == one_attribute
+
+    with subtests.test(msg='assets with two attributes'):
+        result = combine_two_asset_instances(asset1=two_attributes_2, asset2=two_attributes)
+        assert create_dict_from_asset(result) == {
+            'toestand': 'in-gebruik',
+            'testStringField': 'naam',
+            'isActief': True,
+            'assetId': {'identificator': '1'},
+            'typeURI': 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+        }
+
+    with subtests.test(msg='assets with identical attributes'):
+        result = combine_two_asset_instances(asset1=other_type, asset2=other_type_2, allow_attribute_overrides=False)
+
+    with subtests.test(msg='assets with different attributes'):
+        with pytest.raises(ValueError):
+            combine_two_asset_instances(asset1=two_attributes_2, asset2=two_attributes, allow_attribute_overrides=False)
+
+def test_combine_assets(subtests):
+    asset1_1 = AllCasesTestClass()
+    asset1_1.assetId.identificator = '1'
+    asset1_1.testStringField = 'string 2'
+
+    asset1_2 = AllCasesTestClass()
+    asset1_2.assetId.identificator = '1'
+    asset1_2.testStringField = 'string 1'
+
+    asset1_3 = AllCasesTestClass()
+    asset1_3.assetId.identificator = '1'
+    asset1_3.testBooleanField = True
+
+    asset2_1 = AllCasesTestClass()
+    asset2_1.assetId.identificator = '2'
+    asset2_1.testStringField = 'string 2'
+
+    with subtests.test(msg='assets with different attribute values'):
+        with pytest.raises(ValueError):
+            combine_assets(asset_list=[asset1_1, asset1_2], allow_attribute_overrides=False)
+
+    with subtests.test(msg='assets with different attribute values  without changing order 2'):
+        result = combine_assets(asset_list=[asset1_1, asset1_2])
+        assert result[0] == asset1_2
+
+    with subtests.test(msg='assets with different attribute values without changing order 2'):
+        result = combine_assets(asset_list=[asset1_2, asset1_1])
+        assert result[0] == asset1_1
+
+    with subtests.test(msg='three assets with different attribute values'):
+        result = combine_assets(asset_list=[asset1_1, asset1_2, asset1_3])
+        assert create_dict_from_asset(result[0]) == {
+            'testStringField': 'string 1',
+            'testBooleanField': True,
+            'assetId': {'identificator': '1'},
+            'typeURI': 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+        }
+
+    with subtests.test(msg='all test assets'):
+        result = combine_assets(asset_list=[asset1_1, asset1_2, asset1_3, asset2_1])
+        assert create_dict_from_asset(result[0]) == {
+            'testStringField': 'string 1',
+            'testBooleanField': True,
+            'assetId': {'identificator': '1'},
+            'typeURI': 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#AllCasesTestClass'
+        }
+        assert result[1] == asset2_1
