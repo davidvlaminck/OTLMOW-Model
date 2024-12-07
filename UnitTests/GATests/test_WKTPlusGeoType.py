ca1abe41356f17a1949dfccf293adf32a968ca20
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-12-07 09:00:24 +0100

    change geometry warning to error (like in DAVIE)

--- UnitTests/GATests/test_WKTPlusGeoType.py
+++ UnitTests/GATests/test_WKTPlusGeoType.py
@@ -1,6 +1,7 @@
 import pytest
 
 from otlmow_model.OtlmowModel.BaseClasses.OTLAsset import OTLAsset
+from otlmow_model.OtlmowModel.Exceptions.WrongGeometryTypeError import WrongGeometryTypeError
 from otlmow_model.OtlmowModel.Exceptions.WrongGeometryWarning import WrongGeometryWarning
 from otlmow_model.OtlmowModel.GeometrieTypes.PuntGeometrie import PuntGeometrie
 from otlmow_model.OtlmowModel.GeometrieTypes.VlakGeometrie import VlakGeometrie
@@ -28,7 +29,7 @@ def test_point(subtests):
         assert puntclass.geometry is not None
 
     with subtests.test(msg='invalid points'):
-        with pytest.warns(WrongGeometryWarning):
+        with pytest.raises(WrongGeometryTypeError):
             puntclass.geometry = 'POINT (200001 200002)'
         with pytest.raises(ValueError):
             puntclass.geometry = 'POINT Z (200001 200002,0 3)'
@@ -52,19 +53,19 @@ def test_point_polygon(subtests):
         assert puntvlakclass.geometry is not None
 
     with subtests.test(msg='invalid points'):
-        with pytest.warns(WrongGeometryWarning):
+        with pytest.raises(WrongGeometryTypeError):
             puntvlakclass.geometry = 'POINT (200001 220000)'
         with pytest.raises(ValueError):
             puntvlakclass.geometry = 'POINT Z (200001 200002,0 3)'
 
     with subtests.test(msg='invalid points (outside box)'):
-        with pytest.warns(WrongGeometryWarning):
+        with pytest.raises(WrongGeometryTypeError):
             puntvlakclass.geometry = 'POINT (200001 200002)'
         with pytest.raises(ValueError):
             puntvlakclass.geometry = 'POINT Z (1 2,0 3)'
 
     with subtests.test(msg='invalid polygons'):
-        with pytest.warns(WrongGeometryWarning):
+        with pytest.raises(WrongGeometryTypeError):
             puntvlakclass.geometry = 'POLYGON ((200010 200020, 200030 200040, 200050 200060))'
         with pytest.raises(ValueError):
             puntvlakclass.geometry = 'POLYGON Z ((200010.0 200020.0, 200030.0 200040.0 2, 200050.0 200060.0))'
@@ -72,8 +73,8 @@ def test_point_polygon(subtests):
 
 def test_invalid_geometry_based_on_geometry_artefact():
     instance = PointPolygonTestClass()
-    with pytest.warns(WrongGeometryWarning) as wrong_geometry_warning:
+    with pytest.raises(WrongGeometryTypeError) as wrong_geometry_exception:
         instance.geometry = 'LINESTRING Z (200001 200002 10, 200003 200004 20)'
     expected_msg = "Asset type PointPolygonTestClass shouldn't be assigned a LINESTRING Z as geometry, " \
                    "valid types are POINT Z and POLYGON Z"
-    assert expected_msg == wrong_geometry_warning[0].message.args[0]
+    assert expected_msg == wrong_geometry_exception.value.args[0]
